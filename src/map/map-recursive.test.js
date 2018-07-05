31ded2de6eb47ad58531cf1450301809613528ea
Paulo Reis <paulojrn82@gmail.com>
2018-07-05 12:18:42 -0300

    Add tests TDD  in jest

--- src/map/map-recursive.test.js
+++ src/map/map-recursive.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import { expect } from 'chai'
-import map from './map'
+import map from './map-recursive'
 
 it('map should be a function', () => {
   expect(map).to.be.a('function')
@@ -38,57 +38,3 @@ it('map() should return []', () => {
 it('map([1, 2]) should return [1, 2]', () => {
   expect(map([1, 2])).to.be.deep.equal([1, 2])
 })
-
-it('map(1) should throw a TypeError', () => {
-  let error
-
-  try {
-    map(1)
-  } catch (e) {
-    error = e
-  }
-
-  expect(error.name).to.be.equal('TypeError')
-})
-
-it('map(1) should throw a TypeError with message "The first parameter must be an array"', () => {
-  let error
-
-  try {
-    map(1)
-  } catch (e) {
-    error = e
-  }
-
-  expect(error.message).to.be.equal('The first parameter must be an array')
-})
-
-it('map([1, 2], "function") should throw a TypeError', () => {
-  let error
-
-  try {
-    map([1, 2], 'function')
-  } catch (e) {
-    error = e
-  }
-
-  expect(error.name).to.be.equal('TypeError')
-})
-
-it('map([1, 2], "function") should throw a TypeError with message "The second parameter must be a function"', () => {
-  let error
-
-  try {
-    map([1, 2], 'function')
-  } catch (e) {
-    error = e
-  }
-
-  expect(error.message).to.be.equal('The second parameter must be a function')
-})
-
-
-
-
-
-
